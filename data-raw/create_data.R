88846507d550a0fa2e02934520655fb4302df2a1
abdus <abdusoli26@gmail.com>
2024-04-22 20:42:48 -0500

    second commit

---
+++ data-raw/create_data.R
@@ -0,0 +1,17 @@
+## code to prepare `DATASET` dataset goes here
+test <- list()
+
+test$title <- "Test Your Personality6"
+test$statements <- trimws(readLines("statements.md"))
+
+test$scale <- data.frame(
+  label = c(
+    "Very Inaccurateb",
+    "Moderately Inaccurate",
+    "Neither Accurate Nor Inaccurate",
+    "Moderately Accurate",
+    "Very Accurate"
+  ),
+  value = 1:5
+)
+usethis::use_data(test, overwrite = TRUE)
